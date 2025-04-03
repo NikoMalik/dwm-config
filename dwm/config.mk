a71b4b8b18258c0e1eea154d466b5e68e5c883b2
NikoMalik <NikoMalik@users.noreply.github.com>
2025-04-03 16:25:03 +0300

    update some flags

--- dwm/config.mk
+++ dwm/config.mk
@@ -27,11 +27,11 @@ MIMALLOCFLAGS = -DMI_SECURE=0 \
                 -DMI_LOCAL_DYNAMIC_TLS=0 \
                 -DMI_USE_ENVIRON=0 \
                 -DMI_NO_THP=1 \
-                -DMI_PADDING=0 \
                 -DMI_XMALLOC=0 \
                 -DMI_ASAN=0 \
                 -DMI_TRACK_VALID=0 \
-                -DMI_VALGRIND=0
+                -DMI_VALGRIND=0 \
+                -DMIMALLOC_EAGER_COMMIT=0
 
                 
 # freetype
@@ -46,7 +46,7 @@ INCS = -I${X11INC} -I${FREETYPEINC} -I${MIMALLOC_INC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}  -L${MIMALLOC_LIB} -lmimalloc
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${MIMALLOCFLAGS} -DMI_MALLOC_OVERRIDE=1
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${MIMALLOCFLAGS} 
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -fno-plt -flto=auto   -march=native -O3 -pedantic -Wall -Wno-deprecated-declarations  ${INCS} ${CPPFLAGS} 
 LDFLAGS  = ${LIBS} -Wl,-rpath=${MIMALLOC_LIB} 
